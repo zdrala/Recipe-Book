a213778f8f4684ad30fc53715f8f06ac5a166979
Ahmed Ždralović <ahmed.zdralovic@edu.fit.ba>
2021-01-02 12:34:29 +0100

    adding project components and filling with content

---
+++ RecipeBook/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+ 
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+recipes: Recipe []=[
+  new Recipe('A Test Recipe','This is simply a test','https://www.eatwell101.com/wp-content/uploads/2019/04/chicken-and-asparagus-skillet-recipe-2.jpg')
+];
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
